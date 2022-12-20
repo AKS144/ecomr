62524cd4f45e6d5acf6e3cd39b1a275c61e843b7
AKS <abhi144k@gmail.com>
2022-12-20 22:16:26 +0530

    second commit

--- src/components/frontend/auth/Register.js
+++ src/components/frontend/auth/Register.js
@@ -6,8 +6,40 @@ function Register() {
     return(
         <div>
             <Navbar />
-              <h1>Register</h1>
-              <button type="" className="btn btn-primary">Submit</button>
+              <div className="container py-5">
+                <div className="row justify-content-center">
+                    <div className="col-md-6">
+                        <div className="card">
+                            <div className="card-header">
+                                <h4>Register</h4>
+                            </div>
+                            <div className="card-body">
+                                <form>
+                                    <div className="form-group mb-3">
+                                        <label>Full Name</label>
+                                        <input type="text" name="name" className="form-control" value="" />
+                                    </div>
+                                    <div className="form-group mb-3">
+                                        <label>Email Id</label>
+                                        <input type="email" name="email" className="form-control" value="" />
+                                    </div>
+                                    <div className="form-group mb-3">
+                                        <label>Password</label>
+                                        <input type="password" name="password" className="form-control" value="" />
+                                    </div>
+                                    <div className="form-group mb-3">
+                                        <label>Confirm Password</label>
+                                        <input type="password" name="confirm_password" className="form-control" value="" />
+                                    </div>
+                                    <div className="form-group mb-3">
+                                      <button type="submit" className="btn btn-primary">Register</button>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+              </div>
         </div>
       
       
