3573cc2508549c79d76d505ea5a96c7a90b04869
AKS <abhi144k@gmail.com>
2022-12-20 21:24:31 +0530

    first commit

---
+++ src/components/frontend/auth/Login.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Navbar from '../../../layouts/frontend/Navbar';
+
+
+function Login() {
+    return(
+        <div>
+            <Navbar />
+            <h1>Login</h1>
+        </div>
+       
+    )
+}
+
+export default Login; 
---
+++ src/components/frontend/auth/Register.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Navbar from '../../../layouts/frontend/Navbar';
+
+
+function Register() {
+    return(
+        <div>
+            <Navbar />
+              <h1>Register</h1>
+              <button type="" className="btn btn-primary">Submit</button>
+        </div>
+      
+      
+    )
+}
+
+export default Register; 
