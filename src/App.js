c157fd1de8e2da2ce520be5e9d9d4ce19cc4fa08
AKS <abhi144k@gmail.com>
2022-12-25 19:57:54 +0530

    25th commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import {BrowserRouter as Router, Redirect, Route, Switch} from 'react-router-dom';
+import AdminPrivateRoute from './AdminPrivateRoute';
 
 import MasterLayout from './layouts/admin/MasterLayout';
 import Home from './components/frontend/Home';
@@ -14,18 +15,32 @@ axios.defaults.headers.post['Accept'] = 'application/json';
 
 axios.defaults.withCredentials = true;//cors for csrf token
 
+//bearer tokens part 5 for logout
+axios.interceptors.request.use(function (config){
+  const token = localStorage.getItem('auth_token');
+  config.headers.Authorization = token ? `Bearer ${token}` : '';  //maintain space
+  return config;
+});
+
 
 function App() {
   return (
     <div className="App">
      <Router>
       <Switch>
-
         <Route exact path="/" component={Home} />
-        <Route path="/login" component={Login} />
-        <Route path="/register" component={Register} />
+        {/* <Route path="/login" component={Login} />
+        <Route path="/register" component={Register} /> */}
+        <Route path="/login">        
+          {localStorage.getItem('auth_token') ? <Redirect to='/' /> : <Login />}  
+        </Route>
+        <Route path="/register">
+          {/* if else condition in react */}
+          {localStorage.getItem('auth_token') ? <Redirect to='/' /> : <Register />}  
+        </Route>
 
-        <Route path="/admin" name="Admin" render={(props) => <MasterLayout {...props}/>} />
+        {/* <Route path="/admin" name="Admin" render={(props) => <MasterLayout {...props}/>} /> */}
+        <AdminPrivateRoute path="/admin" name="Admin" />
 
       </Switch>
      </Router>
